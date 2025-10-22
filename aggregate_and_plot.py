9179e7404648b2388232cf12b218fdcd73c2a6f8
SeyedAmirHosseini00 <seyedamirhosseini00@gmail.com>
2025-10-23 04:33:41 +0900

    change aggregate to plot for sweep

--- aggregate_and_plot.py
+++ aggregate_and_plot.py
@@ -7,7 +7,7 @@ import re
 
 file_dir = os.path.dirname(__file__)
 log_folder = 'exp_pebble_mixture_alpha_sum_log_over'
-env_name = 'walker_walk'
+env_name = 'metaworld_sweep-into-v2'
 test = None
 results_dir = 'results'
 check_all_tests = True
@@ -95,7 +95,7 @@ def plot_metrics(grouped, metrics, x_values, csv_type, dfs=None, csv_files=None,
 # Process and plot for train.csv, reward.csv, and eval.csv
 if test is None:
     target_log = os.path.join(file_dir, log_folder, env_name)
-    test_dirs = [d for d in glob.glob(os.path.join(target_log, '*5000*')) if os.path.isdir(d)]
+    test_dirs = [d for d in glob.glob(os.path.join(target_log, '*')) if os.path.isdir(d)]
     for test_dir in test_dirs:
         test = os.path.basename(test_dir)
         print(f'Processing test: {test}')
